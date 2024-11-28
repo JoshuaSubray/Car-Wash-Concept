b53c0919b80e949df654b3a99e439718af36ec4f
shelovesJ <jamesgriffiths23@hotmail.com>
2024-11-28 11:46:38 -0500

    Fixing login redirection and database connection issues

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
-// const uri = 'mongodb+srv://jamesgriffiths23:1054g087@central.r9mof.mongodb.net/userAuth';
-const uri = 'mongodb+srv://N01645357:vNTX679I1r7tAzj8@cluster.ohskr.mongodb.net/userAuth';
+const uri = 'mongodb+srv://jamesgriffiths23:1054g087@central.r9mof.mongodb.net/userAuth';
+//const uri = 'mongodb+srv://N01645357:vNTX679I1r7tAzj8@cluster.ohskr.mongodb.net/userAuth';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB.'))
   .catch(err => console.error('Error connecting to MongoDB.', err));
 
-// const loginUri = 'mongodb+srv://jamesgriffiths23:1054g087@central.r9mof.mongodb.net/loginDB';
-const loginUri = 'mongodb+srv://N01645357:vNTX679I1r7tAzj8@cluster.ohskr.mongodb.net/loginDB';
+ const loginUri = 'mongodb+srv://jamesgriffiths23:1054g087@central.r9mof.mongodb.net/userlogins';
 const loginConnection = mongoose.createConnection(loginUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 loginConnection.on('connected', () => console.log('Connected to MongoDB Login.'));
